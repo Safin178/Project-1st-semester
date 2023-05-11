f94da79ca234e5f25bc68c895da9fb59f511daf5
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2023-05-11 09:32:49 +0600

    2

--- main.c
+++ main.c
@@ -3,29 +3,38 @@
 #include<windows.h>
 //Ignore THIS PART....
 void gotoxy(int x, int y)
-  {
-        COORD coord;
-        coord.X = x;
-        coord.Y = y;
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord); 
- }
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 //  this part is copied from Google..
 
-  int  main()
-   {
-        int choice;
-          system("CLS");
-       printf("1 . Login\n\n");
-       printf("2 . Sign up\n\n");
-       printf("3 . Admin \n\n");
-       printf("4 . Exit\n\n");
-        printf("Please Enter a Command >");
-        scanf("%d", &choice);
-        while(choice)
+int  main()
+{
+    int choice;
+
+    printf("1 . Login\n\n");
+    printf("2 . Sign up\n\n");
+    printf("3 . Admin \n\n");
+    printf("4 . Exit\n\n");
+    printf("Please Enter a Command >");
+    scanf("%d", &choice);
+    while(choice)
+    {
+        switch (choice)
         {
-            
+        case 1:
+            login();
+            break;
+        case 2:
+            sign();
+            break;
+        case 3:
+            admin();
+            break;
         }
-        
 
 
 
@@ -35,5 +44,21 @@ void gotoxy(int x, int y)
 
 
 
-return 0;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    return 0;
 }
